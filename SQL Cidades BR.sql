2ec395da4bb2dcb392b7a063ecc4ea3a675d42e4
th14go <thiagoolopezz@gmail.com>
2018-03-26 14:10:50 -0300

    Update SQL Cidades BR.sql

--- SQL Cidades BR.sql
+++ SQL Cidades BR.sql
@@ -3,43 +3,59 @@
 -- Pastebin: http://pastebin.com/Fs2Cb8yS
 -- Twitter: @Thiagoolopezz
 -- Email: thiagoolopezz@gmail.com
--- Atenção para as nomenclaturas “Maiúsculas e Minúsculas”. --
+-- Atenção para as nomenclaturas “Maiúsculas e Minúsculas”.
 
 
 
-INSERT INTO `estado` (`idestado`, `estado`, `uf`) VALUES
-(1, 'Acre', 'AC'),
-(2, 'Alagoas', 'AL'),
-(3, 'Amazonas', 'AM' ),
-(4, 'Amapá', 'AP'),
-(5, 'Bahia', 'BA'),
-(6, 'Ceará', 'CE'),
-(7, 'Distrito Federal', 'DF'),
-(8, 'Espírito Santo', 'ES'),
-(9, 'Goiás', 'GO'),
-(10, 'Maranhão', 'MA'),
-(11, 'Minas Gerais', 'MG'),
-(12, 'Mato Grosso do Sul', 'MS'),
-(13, 'Mato Grosso', 'MT'),
-(14, 'Pará', 'PA'),
-(15, 'Paraíba', 'PB'),
-(16, 'Pernambuco', 'PE'),
-(17, 'Piauí', 'PI'),
-(18, 'Paraná', 'PR'),
-(19, 'Rio de Janeiro', 'RJ'),
-(20, 'Rio Grande do Norte', 'RN'),
-(21, 'Rondônia', 'RO'),
-(22, 'Roraima', 'RR'),
-(23, 'Rio Grande do Sul', 'RS'),
-(24, 'Santa Catarina', 'SC'),
-(25, 'Sergipe', 'SE'),
-(26, 'São Paulo', 'SP'),
-(27, 'Tocantins', 'TO');
+CREATE TABLE IF NOT EXISTS `estado` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(75) DEFAULT NULL,
+  `uf` varchar(5) DEFAULT NULL,
+  `pais` int(7) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_Estado_pais` (`pais`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=28 ;
 
 
+INSERT INTO `estado` (`id`, `nome`, `uf`, `pais`) VALUES
+(1, 'Acre', 'AC', 1),
+(2, 'Alagoas', 'AL', 1),
+(3, 'Amazonas', 'AM', 1),
+(4, 'Amapá', 'AP', 1),
+(5, 'Bahia', 'BA', 1),
+(6, 'Ceará', 'CE', 1),
+(7, 'Distrito Federal', 'DF', 1),
+(8, 'Espírito Santo', 'ES', 1),
+(9, 'Goiás', 'GO', 1),
+(10, 'Maranhão', 'MA', 1),
+(11, 'Minas Gerais', 'MG', 1),
+(12, 'Mato Grosso do Sul', 'MS', 1),
+(13, 'Mato Grosso', 'MT', 1),
+(14, 'Pará', 'PA', 1),
+(15, 'Paraíba', 'PB', 1),
+(16, 'Pernambuco', 'PE', 1),
+(17, 'Piauí', 'PI', 1),
+(18, 'Paraná', 'PR', 1),
+(19, 'Rio de Janeiro', 'RJ', 1),
+(20, 'Rio Grande do Norte', 'RN', 1),
+(21, 'Rondônia', 'RO', 1),
+(22, 'Roraima', 'RR', 1),
+(23, 'Rio Grande do Sul', 'RS', 1),
+(24, 'Santa Catarina', 'SC', 1),
+(25, 'Sergipe', 'SE', 1),
+(26, 'São Paulo', 'SP', 1),
+(27, 'Tocantins', 'TO', 1);
 
+CREATE TABLE IF NOT EXISTS `cidade` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(120) DEFAULT NULL,
+  `estado` int(5) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_Cidade_estado` (`estado`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5565 ;
 
-INSERT INTO `cidade` (`idcidade`, `nomecidade`, `estado_idestado`) VALUES
+INSERT INTO `cidade` (`id`, `nome`, `estado`) VALUES
+
 (1, 'Afonso Cláudio', 8),
 (2, 'Água Doce do Norte', 8),
 (3, 'Águia Branca', 8),
